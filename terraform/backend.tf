eafb216ac621a812f2e0af86126a1cb9b3e96738
Shiva Velpula <codefob@outlook.com>
2026-05-19 21:09:44 +0000

    update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateaksdemo"
+    resource_group_name  = "cloud-shell"
+    storage_account_name = "cs710032000b775b1d7"
     container_name       = "tfstate"
     key                  = "aks.tfstate"
-    use_azuread_auth     = true   # ✅ important for OIDC
+    use_azuread_auth     = true
   }
 }
