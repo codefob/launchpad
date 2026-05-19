f2b898d5b85353db643b46f0a6cda09838403a3f
Shiva Velpula <codefob@outlook.com>
2026-05-19 21:38:41 +0000

    AKS cluster deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,51 @@
+#---Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "AKSLAB_2026"
+  name     = "lab2026"
   location = "centralus"
 }
 
+#---AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-529"
+  name                = "aks-github-actions"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aksdemo"
+  dns_prefix          = "aksgithub"
 
   default_node_pool {
-    name       = "system-nodepool"
+    name       = "sysnp"
     node_count = 1
     vm_size    = "Standard_DS2_v2"
-     mode                  = "system"
-  }
-
-  
-resource "azurerm_kubernetes_cluster_node_pool" "userpool" {
-  name                  = "workernp"  
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_DS2_v2"
-  node_count            = 1
-  mode                  = "User"      
-
-  orchestrator_version  = azurerm_kubernetes_cluster.aks.kubernetes_version
-}
-
-
-tags = {
-    environment = "dev"
+    mode       = "System"
   }
 
   identity {
     type = "SystemAssigned"
+  }
+
+  #--network_plugin
+  network_profile {
+    network_plugin      = "azure"
+    load_balancer_sku   = "standard"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+#---user nodepool creation 
+resource "azurerm_kubernetes_cluster_node_pool" "userpool" {
+  name                  = "usernp01"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+
+  vm_size    = "Standard_DS2_v2"
+  node_count = 1
+
+  mode = "User" 
+
+  orchestrator_version = azurerm_kubernetes_cluster.aks.kubernetes_version
+
+  tags = {
+    environment = "dev"
   }
 }
