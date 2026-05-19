ecc19c96c610442ab20fcaaba578cc9a4e2e9a09
Shiva Velpula <codefob@outlook.com>
2026-05-19 20:59:05 +0000

    update Terraform AKS deployment workflow

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-  use_oidc = true 
+  use_oidc = true
 }
