c084621b212d9445aa106541a42fff3986762d0e
Shiva Velpula <codefob@outlook.com>
2026-05-20 05:07:37 +0000

    Fix ACR build step002

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,16 @@
-#---Resource Group
+# ✅ Resource Group (AKS)
 resource "azurerm_resource_group" "rg" {
   name     = "lab2026"
   location = "centralus"
 }
 
-#---AKS Cluster
+#  Existing ACR (UPDATED RG)
+data "azurerm_container_registry" "acr" {
+  name                = "codefob"
+  resource_group_name = "acr-2025"   
+}
+
+# AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-github-actions"
   location            = azurerm_resource_group.rg.location
@@ -15,36 +21,52 @@ resource "azurerm_kubernetes_cluster" "aks" {
     name       = "sysnp"
     node_count = 1
     vm_size    = "Standard_DS2_v2"
+    mode       = "System"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  #--network_plugin
   network_profile {
-    network_plugin      = "azure"
-    load_balancer_sku   = "standard"
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
   }
 
   tags = {
     environment = "dev"
   }
+
+  lifecycle {
+    ignore_changes = [
+      default_node_pool[0].node_count
+    ]
+  }
 }
 
-#---user nodepool creation 
+# User Node Pool
 resource "azurerm_kubernetes_cluster_node_pool" "userpool" {
   name                  = "usernp01"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
 
   vm_size    = "Standard_DS2_v2"
   node_count = 1
-
-  mode = "User" 
+  mode       = "User"
 
   orchestrator_version = azurerm_kubernetes_cluster.aks.kubernetes_version
+}
 
-  tags = {
-    environment = "dev"
-  }
+#--ACR Pull Permission pull images 
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = data.azurerm_container_registry.acr.id
+}
+
+
+# --- AKS integration with ACR registry
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = data.azurerm_container_registry.acr.id
 }
